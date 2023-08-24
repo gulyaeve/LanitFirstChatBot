9d21523adc180943cd6246dc3efb2e4b70879f47
gulyaeve <gulyaev.e@icloud.com>
2023-08-24 10:22:02 +0300

    Added logging

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
 from handlers import start, any_text, sticker, hello
 from config import settings
 
 
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=settings.TOKEN)
+# Объект диспетчера
+dp = Dispatcher()
+
+# Добавляем роутеры из хэндлеров в диспетчер
+dp.include_router(start.router)
+dp.include_router(sticker.router)
+dp.include_router(hello.router)
+dp.include_router(any_text.router)
+
+
 async def main():
-    # Объект бота
-    bot = Bot(token=settings.TOKEN)
-    # Объект диспетчера
-    dp = Dispatcher()
-
-    # Добавляем роутеры из хэндлеров в диспетчер
-    dp.include_router(start.router)
-    dp.include_router(sticker.router)
-    dp.include_router(hello.router)
-    dp.include_router(any_text.router)
-
     await dp.start_polling(bot)
 
 
